c0f74c9d6512cfe844dd7e482f84a13dfd8a559b
Bruneizer <carlosbruno3856@gmail.com>
2024-10-10 16:02:16 -0300

    Luis se la come
    Agregacion de grants

--- scripts bd/DCL.sql
+++ scripts bd/DCL.sql
@@ -1,2 +1,4 @@
 CREATE USER 'Admin'@'LocalHost' IDENTIFIED BY '12345678';
 Create User 'Usuario'@'%' IDENTIFIED BY 'holaUpwu';
+grant all on Cuenta.* to 'Admin'@'LocalHost';
+grant {select} on {Cuenta} to 'Usuario'@'%';
