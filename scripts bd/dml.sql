e4803c96aae5a25c21d0f0210d3f790f01245a0b
torren007 <torrenprietoruben@gmail.com>
2024-11-06 15:18:41 -0300

    Transaccciones [denuevo[

--- scripts bd/dml.sql
+++ scripts bd/dml.sql
@@ -1,35 +1,15 @@
--- Active: 1691412339871@@127.0.0.1@3306@5to_RiotGames
-BEGIN TRANSACTION;
+START TRANSACTION;
 
-BEGIN TRY
-    -- Paso 1: Declarar variables
-    DECLARE @nuevo_usuario NVARCHAR(50) = 'Summoner1';
-    DECLARE @nivel INT = 1;
-    DECLARE @esencia_azul INT = 500;
-    DECLARE @puntos_riot INT = 100;
-    DECLARE @puntos_liga INT;
-    DECLARE @id_rango BYTE;
-    
-    -- Paso 2: Seleccionar puntos de liga y ID de rango desde la tabla de rangos de League of Legends
-    SELECT @puntos_liga = PuntosLigaNecesarios, @id_rango = IdRango
-    FROM RangoLol
-    WHERE Nombre = 'Plata';  -- Suponiendo que queremos asignar el rango "Plata"
+SELECT @TotalPuntosRiot := SUM(PuntosRiot) 
+FROM 5to_RiotGames.CuentaLol 
+WHERE idCuenta = 0; 
 
-    -- Paso 3: Insertar la nueva cuenta en cuentas_lol con los valores seleccionados
-    INSERT INTO CuentaLol (Nombre, nivel, esencia_azul, puntos_riot, puntos_liga, id_rango)
-    VALUES (@nuevo_usuario, @nivel, @esencia_azul, @puntos_riot, @puntos_liga, @id_rango);
+UPDATE 5to_RiotGames.CuentaLol
+SET PuntosLiga = PuntosLiga + 50, EsenciaAzul = EsenciaAzul + 20
+WHERE idCuenta = 0;
 
-    -- Confirmar la transacción
-    COMMIT;
-    PRINT 'Transacción completada exitosamente.';
+UPDATE 5to_RiotGames.CuentaValorant
+SET Experiecia = Experiecia + 100, PuntosCompetitivo = PuntosCompetitivo + 20
+WHERE idCuenta = 1;
 
-END TRY
-BEGIN CATCH
-    -- Si ocurre un error, revertir la transacción
-    ROLLBACK;
-    PRINT 'Ocurrió un error en la transacción. Se ha revertido.';
-    THROW;
-END CATCH;
-
--- Consultar los valores para verificar la inserción
-SELECT * FROM CuentaLol;
+COMMIT;
