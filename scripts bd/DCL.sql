b691240b0a35e6c7ac42e14231d4cb96ccc22401
Armoaaa <armoaalaea@gmail.com>
2024-11-21 14:21:15 -0300

    Update DCL.sql

--- scripts bd/DCL.sql
+++ scripts bd/DCL.sql
@@ -1,6 +1,6 @@
-
-CREATE USER 'AdminRiot'@'LocalHost' IDENTIFIED BY 'PeruChile123!';
-Create User 'Usuario1'@'%' IDENTIFIED BY 'ChilePeru123!';
-grant UPDATE,SELECT,INSERT on Cuenta.* to 'AdminRiot'@'LocalHost';
-grant select on Cuenta.* to 'Usuario1'@'%';
+delimiter ;
+CREATE USER if not EXISTS 'AdminRiot'@'localhost' IDENTIFIED BY 'PeruChile123!';
+Create User if not EXISTS 'Usuario1'@'%' IDENTIFIED BY 'ChilePeru123!';
+grant UPDATE,SELECT,INSERT on 5to_RiotGames.* to 'AdminRiot'@'localhost';
+grant select on 5to_RiotGames.* to 'Usuario1'@'%';
 
