b99e4273c7476d97c9b25777f35b45187ba921f5
Bruneizer-xd-11 <carlosbruno3856@gmail.com>
2025-11-25 15:22:37 -0300

    arreglo de cosas esteticas

--- scripts bd/dml.sql
+++ scripts bd/dml.sql
@@ -1,5 +1,4 @@
 START TRANSACTION;
-
 -- Reducir PuntosRiot de la cuenta de origen (evitando negativos)
 UPDATE 5to_RiotGames.CuentaLol
 SET PuntosRiot = 
@@ -9,20 +8,15 @@ SET PuntosRiot =
     END
 WHERE idCuenta = 1;
 
--- Incrementar PuntosRiot en la cuenta de destino
 UPDATE 5to_RiotGames.CuentaLol
 SET PuntosRiot = PuntosRiot + 100
 WHERE idCuenta = 2;
-
 COMMIT;
 
-
--- Aumentar los puntos de liga en la cuenta de LOL (en la tabla CuentaLol)
 UPDATE 5to_RiotGames.CuentaLol
 SET PuntosLiga = PuntosLiga + 29
 WHERE IdRango = 2;
 
--- Si la cuenta también está en CuentaValorant, se actualizan los puntos de competitivo (en la tabla CuentaValorant)
 UPDATE 5to_RiotGames.CuentaValorant
 SET PuntosCompetitivo = PuntosCompetitivo + 20
 WHERE idRango = 1;
